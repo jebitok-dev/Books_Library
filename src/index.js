0ecb53f34cc225c0d772c1ac7ccc15ed6d9f91bb
Jebitok <sharonkosgei4@gmail.com>
2021-04-19 19:26:30 +0300

    Add Changes to JS file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 let myLibrary = [];
 
-function Book(author, title, pages) {
+function Book(author, title, pages, read) {
     this.author = author;
     this.title = title;
     this.pages = pages;
+    this.read = read
 }
 
 function addBookToLibrary() {
-
+    const author = document.getElementById("bookAuthor").value;
+    const title = document.getElementById("bookTitle").value;
+    const pages = document.getElementById("bookPages").value;
+    const read = document.getElementById("isRead").checked;
+    myLibrary.push(new Book(author, title, pages, read));
+    reloadLibrary(myLibrary);
 }
+
+var button = document.createElement("BUTTON");
