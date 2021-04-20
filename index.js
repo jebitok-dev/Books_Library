9dafa89479153b6288b516baf075e77c3669d056
Jebitok <sharonkosgei4@gmail.com>
2021-04-20 13:24:22 +0300

    Fix Linters

--- index.js
+++ index.js
@@ -1,78 +1,78 @@
 let myLibrary = [];
 
 function Book(author, title, pages, read) {
-    this.author = author;
-    this.title = title;
-    this.pages = pages;
-    this.read = read;
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.read = read;
 }
 
 function createAndAppendElement(
-    htmlType,
-    htmlClass,
-    elementToAppendTo,
-    textContent = ""
+  htmlType,
+  htmlClass,
+  elementToAppendTo,
+  textContent = ""
 ) {
-    const elem = document.createElement(htmlType);
-    elem.classList.add(htmlClass);
-    elem.textContent = textContent;
-    elementToAppendTo.appendChild(elem);
-    return elem;
+  const elem = document.createElement(htmlType);
+  elem.classList.add(htmlClass);
+  elem.textContent = textContent;
+  elementToAppendTo.appendChild(elem);
+  return elem;
 }
 
 function createBookCard(book, bookIndex = 0) {
-    const { author, title, pages, read } = book;
-    const bookList = document.getElementById("bookList");
-    const bookCard = createAndAppendElement("div", "bookCard", bookList);
-    bookCard.setAttribute("data-bookIndex", bookIndex);
+  const { author, title, pages, read } = book;
+  const bookList = document.getElementById("bookList");
+  const bookCard = createAndAppendElement("div", "bookCard", bookList);
+  bookCard.setAttribute("data-bookIndex", bookIndex);
 
-    const authorEl = createAndAppendElement(
-        "div",
-        "bookAuthor",
-        bookCard,
-        author
-    );
-    const titleEl = createAndAppendElement("div", "bookTitle", bookCard, title);
-    const pagesEl = createAndAppendElement("div", "bookPages", bookCard, pages);
-    const readEl = createAndAppendElement("div", "bookRead", bookCard, read);
-    readEl.checked = read;
+  const authorEl = createAndAppendElement(
+    "div",
+    "bookAuthor",
+    bookCard,
+    author
+  );
+  const titleEl = createAndAppendElement("div", "bookTitle", bookCard, title);
+  const pagesEl = createAndAppendElement("div", "bookPages", bookCard, pages);
+  const readEl = createAndAppendElement("div", "bookRead", bookCard, read);
+  readEl.checked = read;
 
-    const deleteButton = createAndAppendElement(
-        "button",
-        "deleteButton",
-        bookCard,
-        "Delete book"
-    );
-    deleteButton.addEventListener("click", () => {
-        myLibrary.splice(bookIndex, 1);
-        reloadLibrary(myLibrary);
-    });
+  const deleteButton = createAndAppendElement(
+    "button",
+    "deleteButton",
+    bookCard,
+    "Delete book"
+  );
+  deleteButton.addEventListener("click", () => {
+    myLibrary.splice(bookIndex, 1);
+    reloadLibrary(myLibrary);
+  });
 
-    const toggleRead = createAndAppendElement("input", "toggleRead", bookCard);
-    toggleRead.setAttribute("type", "checkbox");
-    toggleRead.checked = read;
-    toggleRead.addEventListener("change", function () {
-        if (this.checked) {
-            myLibrary[bookIndex].read = true;
-        } else {
-            myLibrary[bookIndex].read = false;
-        }
-        reloadLibrary(myLibrary);
-    });
+  const toggleRead = createAndAppendElement("input", "toggleRead", bookCard);
+  toggleRead.setAttribute("type", "checkbox");
+  toggleRead.checked = read;
+  toggleRead.addEventListener("change", function () {
+    if (this.checked) {
+      myLibrary[bookIndex].read = true;
+    } else {
+      myLibrary[bookIndex].read = false;
+    }
+    reloadLibrary(myLibrary);
+  });
 }
 
 function reloadLibrary(library) {
-    document.querySelector("#bookList").innerHTML = "";
-    library.forEach((book, index) => createBookCard(book, index));
+  document.querySelector("#bookList").innerHTML = "";
+  library.forEach((book, index) => createBookCard(book, index));
 }
 
 function addBookToLibrary() {
-    const author = document.getElementById("userAuthor").value;
-    const title = document.getElementById("userTitle").value;
-    const pages = document.getElementById("userPages").value;
-    const read = document.getElementById("isRead").checked;
-    myLibrary.push(new Book(author, title, pages, read));
-    reloadLibrary(myLibrary);
+  const author = document.getElementById("userAuthor").value;
+  const title = document.getElementById("userTitle").value;
+  const pages = document.getElementById("userPages").value;
+  const read = document.getElementById("isRead").checked;
+  myLibrary.push(new Book(author, title, pages, read));
+  reloadLibrary(myLibrary);
 }
 
 document.getElementById("newBook").addEventListener("click", addBookToLibrary);
